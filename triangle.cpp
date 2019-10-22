996f6172ba7bb31fec54ada8031c04ef1cbda9eb
IllCher <50427226+IllCher@users.noreply.github.com>
2019-10-22 19:21:31 +0300

    update

--- triangle.cpp
+++ triangle.cpp
@@ -11,6 +11,13 @@ triangle::triangle(const point& a, const point& b, const point& c) : _a{a}, _b{b
 }
 triangle::triangle(std::istream& is) {
     is >> _a >> _b >> _c;
+    double l, k, p;
+    l = sqrt((_a.x - _b.x) * (_a.x - _b.x) + (_a.y - _b.y) * (_a.y - _b.y));
+    k = sqrt((_b.x - _c.x) * (_b.x - _c.x) + (_b.y - _c.y) * (_b.y - _c.y));
+    p = sqrt((_c.x - _b.x) * (_c.x - _b.x) + (_c.y - _b.y) * (_c.y - _b.y));
+    if (l + k <= p || l + p <= k || p + k <= l) {
+        throw std::logic_error("Triangle doesn't exist");
+    }
 }
 double triangle::square() const {
     return fabs(((_a.x - _c.x) * (_b.y - _c.y) - (_b.x - _c.x) * (_a.y - _c.y)) / 2);

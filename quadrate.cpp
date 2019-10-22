5a86bf893e04625fbf1d43728a1a1586c71d1b4c
IllCher <50427226+IllCher@users.noreply.github.com>
2019-10-22 13:51:26 +0300

    updated print

--- quadrate.cpp
+++ quadrate.cpp
@@ -1,18 +1,23 @@
 #include "quadrate.h"
-quadrate::quadrate(): _a{0, 0}, _b{0, 0}{}
-quadrate::quadrate(const point& a, const point& b) : _a{a}, _b{b}{}
+quadrate::quadrate(): _a{0, 0}, _c{0, 0}{}
+quadrate::quadrate(const point& a, const point& c) : _a{a}, _c{c}{}
 quadrate::quadrate(std::istream &is) {
-    is >> _a >> _b;
+    is >> _a >> _c;
 };
 double quadrate::square() const {
-    return (_b.x - _a.x) * (_b.x - _a.x);
+    return (_c.x - _a.x) * (_c.x - _a.x);
 }
 point quadrate::center() const {
-    return point{(_a.x + _b.x) / 2, (_a.y + _b.y) / 2};
+    return point{(_a.x + _c.x) / 2, (_a.y + _c.y) / 2};
 }
 std::ostream& quadrate::print(std::ostream& os) const {
     point m = center();
-    os << _a  << _b << std::endl;
+    point b, d;
+    b.x = m.x - _c.y + m.y;
+    b.y = m.y + _c.x - m.x;
+    d.x = m.x - _a.y + m.y;
+    d.y = m.y + _a.x - m.x;
+    os << _a  << b << _c << d << std::endl;
     return os;
 }
 
